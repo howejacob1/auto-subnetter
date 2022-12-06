5b916521692486a55727574987229aeea9c14ca0
Jacob Howe <howejacob1@gmail.com>
2022-12-06 15:09:13 -0500

    started converting program to python

---
+++ auto-subnetter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def main():
+    ip = input("Enter in the provided IP range. Example: 192.168.0.0/24: ")
+    print(ip)
+
+if __name__ == "__main__":
+    main()
+
+# For those of you who is working on the 4 bonuspoint subnet design program (Python)
+# If you are working on this program, please make sure you implement the following regarding input of parameters.
+# These parameters can be put either in a file (name of which becomes the parameters to the program, when you start it),
+# or can be typed in, after the program starts. If the program starts without a file-name input parameter, then the program asks for it.
+# 1st line, base subnet address: a.b.c.d/n
+# 2nd line, subnet sizes in any order: n1, n2 n3 n4, n5   # number are separated either by a comma "," or space " ", or both
+# If the program reads the parameters from a file, then any line starting with a hash "#" is considered a comment and ignored.
+# Once you have completed your program, please set up a time with me to show me in my office.
+# Thank you all and good luck.
+# -PK
